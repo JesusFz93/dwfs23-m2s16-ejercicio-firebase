cf25f284c9a46203e626724e4a26c29277086437
Jesus <jesus.fdz93@gmail.com>
2024-01-06 13:00:26 -0600

    variables de entorno

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAtwnyEJMFHD0C-DqJfJo6beyGXZK1HctA",
-  authDomain: "dwfs23-m2s16.firebaseapp.com",
-  projectId: "dwfs23-m2s16",
-  storageBucket: "dwfs23-m2s16.appspot.com",
-  messagingSenderId: "191000430452",
-  appId: "1:191000430452:web:b8a46988aa730479488e21",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
